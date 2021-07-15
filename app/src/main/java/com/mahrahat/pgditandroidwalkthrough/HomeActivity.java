6ade820fe8cbeeab2a9dd263defda0b0481e51c9
Rahat <fountaein@yandex.com>
2021-07-16 04:25:33 +0600

    C of CRUD

--- app/src/main/java/com/mahrahat/pgditandroidwalkthrough/HomeActivity.java
+++ app/src/main/java/com/mahrahat/pgditandroidwalkthrough/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.mahrahat.pgditandroidwalkthrough;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +35,23 @@ public class HomeActivity extends AppCompatActivity {
                 "pass text" +
                 ")";
         itdb.execSQL(createOp);
+        Button createOpButton = findViewById(R.id.btnInsert);
+        createOpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                ContentValues cv = new ContentValues();
+                EditText etRoll = findViewById(R.id.etRoll);
+                String myRoll = etRoll.getText().toString();
+                cv.put("roll", myRoll);
+                cv.put("name", ((EditText) findViewById(R.id.etName)).getText().toString());
+                cv.put("email", ((EditText) findViewById(R.id.etEmail)).getText().toString());
+                cv.put("pass", ((EditText) findViewById(R.id.etPass)).getText().toString());
+                itdb.insert(tableName, null, cv);
+            }
+        });
+    }
+
+    public void showToast(String str) {
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
     }
 
 }
