fcf6a5ba240dbf77857a7b4b9801e7b580f0fa4a
Rahat <fountaein@yandex.com>
2021-07-15 08:41:41 +0600

    findViewById, R file, OnClickListener

--- app/src/main/java/com/mahrahat/pgditandroidwalkthrough/MainActivity.java
+++ app/src/main/java/com/mahrahat/pgditandroidwalkthrough/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mahrahat.pgditandroidwalkthrough;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEnter = findViewById(R.id.btnEnter);
+        btnEnter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
