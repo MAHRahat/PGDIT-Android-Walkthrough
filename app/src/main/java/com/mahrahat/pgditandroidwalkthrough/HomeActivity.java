6417fbd11ae7b464a26e84c2c4f1ac06d64ca195
Rahat <fountaein@yandex.com>
2021-07-16 02:03:58 +0600

    comment out getIntent()

--- app/src/main/java/com/mahrahat/pgditandroidwalkthrough/HomeActivity.java
+++ app/src/main/java/com/mahrahat/pgditandroidwalkthrough/HomeActivity.java
@@ -12,9 +12,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Bundle bundle = getIntent().getExtras();
-        String s = bundle.getString("passed");
-        TextView tvHome = findViewById(R.id.tvHomeHead);
-        tvHome.setText(s);
+//        Bundle bundle = getIntent().getExtras();
+//        String s = bundle.getString("passed");
+//        TextView tvHome = findViewById(R.id.tvHomeHead);
+//        tvHome.setText(s);
     }
 }
