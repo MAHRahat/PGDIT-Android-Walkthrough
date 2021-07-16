c465c2734d0acc41a16088c661e971ea51234c9d
Rahat <fountaein@yandex.com>
2021-07-16 08:55:09 +0600

    apply custom adapter

--- app/src/main/java/com/mahrahat/pgditandroidwalkthrough/AllInfo.java
+++ app/src/main/java/com/mahrahat/pgditandroidwalkthrough/AllInfo.java
@@ -1,14 +1,35 @@
 package com.mahrahat.pgditandroidwalkthrough;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.List;
 
 public class AllInfo extends AppCompatActivity {
+
+    ListView lvStList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_info);
+
+        List<Student> studentList = null;
+        lvStList = findViewById(R.id.lvAllInfo);
+        lvStList.setAdapter(new StudentAdapter(studentList, this.getApplicationContext()));
+
+        SQLiteDatabase itdb = openOrCreateDatabase("walkthrough.db", MODE_PRIVATE, null);
+        String tableName = "Student";
+
+        Cursor cursor = itdb.query(tableName, null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int readRoll = cursor.getInt(cursor.getColumnIndex("roll"));
+            String readName = cursor.getString(cursor.getColumnIndex("name"));
+            studentList.add(new Student(readRoll, readName));
+        }
     }
 }
