6b3b9453b7c0d8e789139ae7c0195845abe67662
Rahat <fountaein@yandex.com>
2021-07-16 08:02:10 +0600

    add Student class

---
+++ app/src/main/java/com/mahrahat/pgditandroidwalkthrough/Student.java
@@ -0,0 +1,27 @@
+package com.mahrahat.pgditandroidwalkthrough;
+
+public class Student {
+    private int roll;
+    private String name;
+
+    public Student(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
